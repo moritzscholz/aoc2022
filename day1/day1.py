14c10221264e21e970cf3ddb31afd7ded5d10e27
moritz <moritz_scholz_jpn@yahoo.co.jp>
2022-12-02 22:32:57 +0900

    solution: day 1, puzzle 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,11 +1,32 @@
 from pathlib import Path
 
 
-def find_most_calories() -> int:
+def __list_of_calories() -> list[int]:
+    """Reads the input file into blocks of calories in the elve's backpack"""
     input_path = Path(__file__).parent / "input.txt"
     with open(input_path, "r", encoding="utf-8") as fp:
         blocks = fp.read().split("\n\n")
 
     total_calories = [sum(int(c) for c in b.strip().split("\n")) for b in blocks]
 
+    return total_calories
+
+
+def find_most_calories() -> int:
+    """Finds the most calories in the input list."""
+    total_calories = __list_of_calories()
+
     return max(total_calories)
+
+
+def find_topmost_calories(items: int) -> list[int]:
+    """Finds the topmost `items` calories."""
+    top_calories: list(int) = []
+    total_calories = __list_of_calories()
+
+    for _ in range(0, items):
+        (idx, calories) = max(enumerate(total_calories), key=lambda v: v[1])
+        total_calories[idx] = 0
+        top_calories += [calories]
+
+    return top_calories
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from day1.day1 import find_most_calories
+from day1.day1 import find_most_calories, find_topmost_calories
 
 if __name__ == "__main__":
     print("Solutions:")
 
-    print(f"Day 1: {find_most_calories()}")
+    print(f"Day 1, puzzle 1: {find_most_calories()}")
+    print(f"Day 1, puzzle 2: {sum(find_topmost_calories(3))}")
