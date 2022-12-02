0c196f94c82e8346660c1c18122ed9e549e3bfc9
moritz <moritz_scholz_jpn@yahoo.co.jp>
2022-12-02 23:17:15 +0900

    solution: day 2, puzzle 2

--- day2/rock_paper_scissors.py
+++ day2/rock_paper_scissors.py
@@ -62,6 +62,25 @@ class RPSResult(int, Enum):
                 return RPSResult.DRAW
 
     @classmethod
+    def from_character(cls, character: str) -> "RPSResult":
+        """Returns result read from the coded character of the input file
+        X: need to lose
+        Y: need a draw
+        Z: need to win
+        """
+        match character:
+            case "X":
+                return RPSResult.LOST
+            case "Y":
+                return RPSResult.DRAW
+            case "Z":
+                return RPSResult.WON
+
+        raise ValueError(
+            "Given character is not a valid input for result: " + character
+        )
+
+    @classmethod
     def score_from_row(cls, row: str) -> int:
         """
         Parses a coded rock-paper-scissors row and returns the score of the
@@ -77,11 +96,48 @@ class RPSResult(int, Enum):
         return cls.round_result(own, opponent).value + own.score()
 
 
-def puzzle1_score() -> int:
+def choose_move(opponent: RPS, result: RPSResult) -> RPS:
+    """Chooses own move given opponent's move and intended result."""
+    match (result, opponent):
+        case (RPSResult.DRAW, _):
+            return opponent
+        case (RPSResult.WON, RPS.ROCK):
+            return RPS.PAPER
+        case (RPSResult.WON, RPS.PAPER):
+            return RPS.SCISSORS
+        case (RPSResult.WON, RPS.SCISSORS):
+            return RPS.ROCK
+        case (RPSResult.LOST, RPS.ROCK):
+            return RPS.SCISSORS
+        case (RPSResult.LOST, RPS.PAPER):
+            return RPS.ROCK
+        case (RPSResult.LOST, RPS.SCISSORS):
+            return RPS.PAPER
+
+
+def __read_rows() -> list[str]:
     input_path = Path(__file__).parent / "input.txt"
     with open(input_path, "r", encoding="utf-8") as fp:
         rows = fp.readlines()
 
-    print(f"{len(rows)} games.")
+    return rows
 
+
+def puzzle1_score() -> int:
+    rows = __read_rows()
     return sum(RPSResult.score_from_row(row) for row in rows)
+
+
+def puzzle2_score() -> int:
+    rows = __read_rows()
+    score = 0
+
+    for row in rows:
+        game = row.strip().split(" ")
+        opponent = RPS.from_character(game[0])
+        result = RPSResult.from_character(game[1])
+        own = choose_move(opponent, result)
+
+        score += RPSResult.round_result(own, opponent) + own.score()
+
+    return score
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from day1.day1 import find_most_calories, find_topmost_calories
-from day2.rock_paper_scissors import puzzle1_score
+from day2.rock_paper_scissors import puzzle1_score, puzzle2_score
 
 if __name__ == "__main__":
     print("Solutions:")
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     print(f"Day 1, puzzle 2: {sum(find_topmost_calories(3))}")
 
     print(f"Day 2, puzzle 1: {puzzle1_score()}")
+    print(f"Day 2, puzzle 2: {puzzle2_score()}")
