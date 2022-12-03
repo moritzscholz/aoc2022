272a2e68307dc0c50b66de85e70861a5cc4a4fa0
moritz <moritz_scholz_jpn@yahoo.co.jp>
2022-12-03 15:20:06 +0900

    refactor: Organize code in src-folder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from day1.day1 import find_most_calories, find_topmost_calories
-from day2.rock_paper_scissors import puzzle1_score, puzzle2_score
+from src.day1.day1 import find_most_calories, find_topmost_calories
+from src.day2.rock_paper_scissors import puzzle1_score, puzzle2_score
 
 if __name__ == "__main__":
     print("Solutions:")
